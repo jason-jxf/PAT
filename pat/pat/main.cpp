cd5d79ebb022c2527c0039db94cf2060b01e54bd
JXF <jxf@JXFdeMacBook.local>
2017-04-11 13:51:01 +0800

    jxf 4.11

---
+++ pat/pat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pat
+//
+//  Created by JXF on 2017/4/11.
+//  Copyright © 2017年 JXF. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
